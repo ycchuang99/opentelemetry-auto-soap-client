90e5064655081db34ce4e272d0003b3f3aae6a25
Evan Chuang <evanchuang@starlux-airlines.com>
2025-03-25 09:42:01 +0800

    ci: remove release action and format some code

--- src/SoapClientAttributes.php
+++ src/SoapClientAttributes.php
@@ -7,7 +7,10 @@ namespace OpenTelemetry\Contrib\Instrumentation\SoapClient;
 interface SoapClientAttributes
 {
     public const SOAP_LOCATION = 'soap.location';
+
     public const SOAP_ACTION = 'soap.action';
+    
     public const SOAP_VERSION = 'soap.version';
+    
     public const SOAP_ONE_WAY = 'soap.one_way';
 }
