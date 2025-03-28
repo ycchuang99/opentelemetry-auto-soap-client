e509aa073e47bf580b5d3d85cf50d31a6d9a0664
Evan C <ycchuang1999@gmail.com>
2025-03-28 20:21:12 +0800

    Fix SoapClientInstrumentation for PHP 8.2

--- src/SoapClientAttributes.php
+++ src/SoapClientAttributes.php
@@ -9,8 +9,8 @@ interface SoapClientAttributes
     public const SOAP_LOCATION = 'soap.location';
 
     public const SOAP_ACTION = 'soap.action';
-    
+
     public const SOAP_VERSION = 'soap.version';
-    
+
     public const SOAP_ONE_WAY = 'soap.one_way';
 }
