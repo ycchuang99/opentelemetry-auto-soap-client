0224f92b65ee8a9d4ac59bc4699886f16e5fa8a6
Evan Chuang <evanchuang@starlux-airlines.com>
2025-08-14 11:59:33 +0800

    refactor(attribute): modify deprecate attributes but keep missing attribute

--- src/SoapClientInstrumentation.php
+++ src/SoapClientInstrumentation.php
@@ -10,8 +10,14 @@ use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use function OpenTelemetry\Instrumentation\hook;
+use OpenTelemetry\SemConv\Attributes\CodeAttributes;
+use OpenTelemetry\SemConv\Attributes\HttpAttributes;
+use OpenTelemetry\SemConv\Attributes\ServerAttributes;
+use OpenTelemetry\SemConv\Attributes\UrlAttributes;
+use OpenTelemetry\SemConv\Incubating\Attributes\HttpIncubatingAttributes;
 use OpenTelemetry\SemConv\TraceAttributes;
 use SoapClient;
+
 use Throwable;
 
 class SoapClientInstrumentation
@@ -23,7 +29,7 @@ class SoapClientInstrumentation
         $instrumentation = new CachedInstrumentation(
             'io.opentelemetry.contrib.php.soap-client',
             null,
-            'https://opentelemetry.io/schemas/1.30.0',
+            'https://opentelemetry.io/schemas/1.32.0',
         );
 
         hook(
@@ -35,16 +41,15 @@ class SoapClientInstrumentation
 
                 $span = $instrumentation->tracer()->spanBuilder(sprintf('%s::%s', $class, $function))
                     ->setSpanKind(SpanKind::KIND_CLIENT)
-                    ->setAttribute(TraceAttributes::CODE_FUNCTION_NAME, $function)
-                    ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
-                    ->setAttribute(TraceAttributes::CODE_FILE_PATH, $filename)
-                    ->setAttribute(TraceAttributes::CODE_LINE_NUMBER, $lineno)
-                    ->setAttribute(TraceAttributes::HTTP_REQUEST_BODY_SIZE, strlen($request))
-                    ->setAttribute(TraceAttributes::URL_FULL, $location)
-                    ->setAttribute(TraceAttributes::URL_PATH, $url['path'] ?? '')
-                    ->setAttribute(TraceAttributes::URL_QUERY, $url['query'] ?? '')
-                    ->setAttribute(TraceAttributes::URL_SCHEME, $url['scheme'] ?? '')
-                    ->setAttribute(TraceAttributes::SERVER_ADDRESS, $url['host'] ?? '')
+                    ->setAttribute(CodeAttributes::CODE_FUNCTION_NAME, sprintf('%s::%s', $class, $function))
+                    ->setAttribute(CodeAttributes::CODE_FILE_PATH, $filename)
+                    ->setAttribute(CodeAttributes::CODE_LINE_NUMBER, $lineno)
+                    ->setAttribute(HttpIncubatingAttributes::HTTP_REQUEST_BODY_SIZE, strlen($request))
+                    ->setAttribute(UrlAttributes::URL_FULL, $location)
+                    ->setAttribute(UrlAttributes::URL_PATH, $url['path'] ?? '')
+                    ->setAttribute(UrlAttributes::URL_QUERY, $url['query'] ?? '')
+                    ->setAttribute(UrlAttributes::URL_SCHEME, $url['scheme'] ?? '')
+                    ->setAttribute(ServerAttributes::SERVER_ADDRESS, $url['host'] ?? '')
                     ->setAttribute(SoapClientAttributes::SOAP_ACTION, $action)
                     ->setAttribute(SoapClientAttributes::SOAP_VERSION, $version)
                     ->setAttribute(SoapClientAttributes::SOAP_ONE_WAY, $oneWay)
@@ -52,7 +57,7 @@ class SoapClientInstrumentation
                 
                 $headers = $soapClient->__getLastRequestHeaders();
                 if ($headers) {
-                    $span->setAttribute(TraceAttributes::HTTP_REQUEST_HEADER, $headers);
+                    $span->setAttribute(HttpAttributes::HTTP_REQUEST_HEADER, $headers);
                 }
 
                 Context::storage()->attach($span->storeInContext(Context::getCurrent()));
@@ -68,11 +73,11 @@ class SoapClientInstrumentation
                 
                 if ($responseHeaders) {
                     $span->setAttribute(TraceAttributes::NETWORK_PROTOCOL_VERSION, self::extractHttpVersion($responseHeaders))
-                        ->setAttribute(TraceAttributes::HTTP_RESPONSE_STATUS_CODE, self::extractHttpStatusCode($responseHeaders));
+                        ->setAttribute(HttpAttributes::HTTP_RESPONSE_STATUS_CODE, self::extractHttpStatusCode($responseHeaders));
                 }
                 
                 if ($result) {
-                    $span->setAttribute(TraceAttributes::HTTP_RESPONSE_BODY_SIZE, strlen($result));
+                    $span->setAttribute(HttpIncubatingAttributes::HTTP_RESPONSE_BODY_SIZE, strlen($result));
                 }
                 
                 self::endSpan($exception);
